4909b6f536c5f4d88de160a4bc0f4409b91524d0
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2022-10-17 17:57:22 +0300

    Added method to delete PipeDrive Api ticket.

--- src/api-tokens/api-tokens.controller.ts
+++ src/api-tokens/api-tokens.controller.ts
@@ -1,18 +1,23 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 import {AddApiTokenDto} from "./dto";
 import {ApiTokensService} from "./api-tokens.service";
 
-@Controller('api-tokens')
+@Controller('apiTokens')
 export class ApiTokensController {
     constructor(private apiTokensService: ApiTokensService) {}
 
-    @Get("get/:mattermostUserId")
-    async getPipeDriveApiToken(@Param("mattermostUserId") mattermostUserId: string) {
+    @Get("/:mattermostUserId")
+    async getApiToken(@Param("mattermostUserId") mattermostUserId: string) {
         return this.apiTokensService.getApiToken(mattermostUserId);
     }
 
-    @Post("register")
-    async registerApiTokens(@Body() dto: AddApiTokenDto) {
+    @Post("")
+    async registerApiToken(@Body() dto: AddApiTokenDto) {
         return this.apiTokensService.registerApiToken(dto);
+    }
+
+    @Delete("/:mattermostUserId")
+    async deleteApiToken(@Param("mattermostUserId") mattermostUserId: string) {
+        return await this.apiTokensService.deleteApiToken(mattermostUserId);
     }
 }
--- src/api-tokens/api-tokens.service.ts
+++ src/api-tokens/api-tokens.service.ts
@@ -3,11 +3,11 @@ import {PrismaService} from "../prisma/prisma.service";
 import {AddApiTokenDto} from "./dto";
 import {PrismaClientKnownRequestError} from "@prisma/client/runtime";
 
+
 @Injectable()
 export class ApiTokensService {
     constructor(private prisma: PrismaService) {
     }
-
     async findApiToken(mattermostUserId: string) {
         const res = await this.prisma.apiToken.findFirst({
             where: {
@@ -17,7 +17,7 @@ export class ApiTokensService {
         return res
     }
 
-
+    // Read token
     async getApiToken(mattermostUserId: string) {
         const  res = this.findApiToken(mattermostUserId);
         if (res == null) {
@@ -29,6 +29,7 @@ export class ApiTokensService {
         return res;
     }
 
+    // Create new token
     async registerApiToken(dto: AddApiTokenDto) {
         // Check if this token is not already in the database
         const res = await this.findApiToken(dto.mattermostUserId);
@@ -53,5 +54,17 @@ export class ApiTokensService {
             throw new BadRequestException("please provide mattermostUserId and pipedriveApiKey fields");
         }
 
+    }
+
+    // Delete token record
+
+    async deleteApiToken(mattermostUserId: string) {
+        // TODO: change deleteMany to delete, because delete returns weird error.
+        const deletedUser = await this.prisma.apiToken.deleteMany({
+            where: {
+                mattermostUserId : mattermostUserId,
+            }
+        })
+        return deletedUser;
     }
 }
